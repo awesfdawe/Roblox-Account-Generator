00375144c3c5c534f387ec8d62436e318967c91d
awesfdawe <124027549+awesfdawe@users.noreply.github.com>
2023-03-04 18:22:34 +0200

    release

---
+++ main.py
@@ -0,0 +1,103 @@
+import os
+import random
+import string
+import json
+from rich.console import Console
+from rich.prompt import IntPrompt
+from playwright.sync_api import sync_playwright
+
+console = Console()
+
+
+def generate_password(length=20):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(alphabet) for _ in range(length))
+
+
+def generate_nickname():
+    # Generates a random nickname by combining a word from the file with a random number
+    try:
+        with open('nicknames.txt', 'r') as file:
+            words = [line.strip() for line in file.readlines()]
+            return random.choice(words) + str(random.randint(10000, 999999))
+    except FileNotFoundError:
+        console.print('[bold red]Error:[/bold red] nicknames.txt file not found!')
+        console.input('Press enter to exit... ')
+        exit()
+
+
+def clear():
+    # Clears the console screen and adds the name at the beginning
+    os.system('cls' if os.name == 'nt' else 'clear')
+    console.rule('[bold red]Roblox Account Generator[/]')
+
+
+def registration():
+    with sync_playwright() as pw:
+        nickname = generate_nickname()
+        password = generate_password()
+        browser = pw.firefox.launch(headless=False)  # * Don't try to use chromium browsers because they got detected by roblox as bot.
+        context = browser.new_context()
+        page = context.new_page()
+        
+        page.route('https://apis.roblox.com/universal-app-configuration/v1/behaviors/cookie-policy/content', lambda route: route.abort()) # Blocks cookies banner
+        page.goto('https://www.roblox.com')
+        page.locator('//*[@id="MonthDropdown"]').select_option('January')
+        page.locator('//*[@id="DayDropdown"]').select_option('01')
+        page.locator('//*[@id="YearDropdown"]').select_option('1999')
+        
+        while True:
+            # Checks username valid
+            with page.expect_response('https://auth.roblox.com/v1/usernames/validate') as username_validate:
+                page.locator('//*[@id="signup-username"]').fill(nickname)
+            if username_validate.value.json().get('code') == 0:
+                break
+            else:
+                nickname = generate_nickname()
+
+        page.locator('//*[@id="signup-password"]').fill(password)
+        page.locator('//*[@id="MaleButton"]').click()
+
+        with page.expect_response('https://auth.roblox.com/v2/signup') as signup_validate:
+            page.locator('//*[@id="signup-button"]').click(timeout=0)
+        if signup_validate.value.status == 429:
+            browser.close()
+            console.log(f'[bold red]Error:[/bold red] too many requests! Try to change your IP (you can use VPN, preferably paid)')
+            console.input('Press enter to continue...')
+            return None
+        else:
+            with page.expect_response('https://client-api.arkoselabs.com/fc/gt2/public_key/**') as signup_validate:
+                pass
+            if signup_validate.value.ok:
+                page.wait_for_url('https://www.roblox.com/home?nu=true', timeout=0)
+            else:
+                browser.close()
+                console.log(f'[bold red]Error:[/bold red] Unknown error! Try to change your IP (you can use VPN, preferably paid)')
+                console.input('Press enter to continue...')
+                return None
+
+        with open('cookies.txt', 'a') as file:
+            cookies = context.cookies()
+            roblosecurity_cookie = next((cookie for cookie in cookies if cookie['name'] == '.ROBLOSECURITY'),None)  # Finds .ROBLOSECURITY cookies
+            if roblosecurity_cookie:
+                roblos_data_value = roblosecurity_cookie['value']
+                file.write(f'{roblos_data_value}\n')
+
+        with open('accounts.txt', 'a') as file:
+            file.write(f'{nickname}:{password}\n')
+        console.log(f'[bold green]Account generated successfully[/bold green]', ':white_check_mark:')
+        browser.close()
+        return None
+
+
+def main():
+    clear()
+    amount = IntPrompt.ask('Enter how many accounts to generate')
+    clear()
+    for _ in range(amount):
+        with console.status('Generating...', spinner='line'):
+            registration()
+
+
+if __name__ == '__main__':
+    main()
